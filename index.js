1cf9522867c0ba076f537742f858bc7a94e74d40
Jonas Haefele <jonas@decoded.co>
2015-10-19 13:05:03 +0100

    more webpack config, trying to make it work both locally and on heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 
+var isProduction = process.env.NODE_ENV === 'production';
 app.set('port', (process.env.PORT || 5000));
 
 /*****************************\
@@ -30,12 +32,42 @@ app.use(function(request, response, next) {
 
 app.use(express.static(__dirname + '/build'));
 
+
+// We only want to run the workflow when not in production
+if (!isProduction) {
+  console.log('Running locally, serving assets from webpack dev server.')
+  var httpProxy = require('http-proxy');
+  var proxy = httpProxy.createProxyServer();
+  // We require the bundler inside the if block because
+  // it is only needed in a development environment. Later
+  // you will see why this is a good idea
+  var bundle = require('./server/bundle.js');
+  bundle();
+
+  // Any requests to localhost:3000/build is proxied
+  // to webpack-dev-server
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    });
+  });
+
+  // It is important to catch any errors from the proxy or the
+  // server will crash. An example of this is connecting to the
+  // server when webpack is bundling
+  proxy.on('error', function(e) {
+    console.log('Could not connect to proxy, please try again...');
+  });
+
+}
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
 
 app.get('/', function(request, response) {
-  response.render('build/index');
+  response.render('pages/home');
 });
 
 app.listen(app.get('port'), function() {
