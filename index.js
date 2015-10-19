ba2e0b8e807e6bc584fd50b39b8162803cc249ae
Jonas Haefele <jonas@decoded.co>
2015-10-19 11:13:05 +0100

    prepare for heroku

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+/*****************************\
+| DO NOT REMOVE THIS FUNCTION |
+\*****************************/
+app.use(function(request, response, next) {
+  var ipAddr = request.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = request.connection.remoteAddress;
+  }
+  // Allow access to labs
+  allowed_ips = ['::1','127.0.0.1','localhost','::ffff:127.0.0.1'];
+  if (ipAddr === process.env.LABS_IP || allowed_ips.indexOf(ipAddr) != -1) {
+    next();
+  } else {
+    response.status(403);
+    response.send('Direct access forbidden');
+    response.end();
+  }
+})
+/*****************\
+| OK, CARRY ON... |
+\*****************/
+
+app.use(express.static(__dirname + '/build'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('build/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
