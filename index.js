e71b4e4b4d3e9b40aa0239158a6acd5a404aa2a7
Jonas Haefele <jonas@decoded.co>
2015-10-26 14:50:01 +0000

    aside nav

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var hbsexpress = require('express-handlebars');
+var handlebarsCore=require('handlebars')
 var marked = require('marked');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
@@ -38,13 +40,38 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/src/views');
 
+routesHelper = function(options) {
+  // get routes
+  data = handlebarsCore.createFrame();
+  var route;
+  data.routes = [];
+  app._router.stack.forEach(function(middleware){
+      if(middleware.route){ // routes registered directly on the app
+          data.routes.push({label: middleware.route.path.replace('/', ' '), link: middleware.route.path, type: 'nav-link'});
+          // routes.push(middleware.route);
+      } else if(middleware.name === 'router'){ // router middleware
+          middleware.handle.stack.forEach(function(handler){
+              route = handler.route;
+              // route && routes.push(route);
+              route && data.routes.push({label: route.path.replace('/', ' '), link: route.path, type: 'nav-link'});
+          });
+      }
+  });
+
+  options.data = data;
+  return options.fn(options.data);
+};
 
 var Handlebars = hbsexpress({
+  handlebars: handlebarsCore,
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir:  app.get('views') + '/layouts',
   partialsDir: [ app.get('views') + '/component-partials', app.get('views') + '/partials'],
-  templatesDir: [ app.get('views') + '/components-templates']
+  templatesDir: [ app.get('views') + '/components-templates'],
+  helpers: {
+    routes: routesHelper,
+    partial: require('./src/js/helpers/dynamicpartial.js')}
 });
 
 // Register `hbs.engine` with the Express app.
@@ -57,7 +84,6 @@ app.get('/', function(request, response) {
 });
 
 // Initialize ALL dynamic routes including subfolders
-var fs = require('fs');
 // read all files and folders in /routes
 function recursiveRoutes(folderName) {
     fs.readdirSync(folderName).forEach(function(file) {
@@ -71,30 +97,31 @@ function recursiveRoutes(folderName) {
         } else if (file.toLowerCase().indexOf('.js') > 1) {
         // if file is js, include it, let the js set up the routes
             require('./' + fullName);
-            console.log("require('" + fullName + "')");
         } else if (file.toLowerCase().indexOf('.md') > 1) {
-        // if the file is markdown, read the file, parse it and render the file in MD template
-          fs.readFile(fullName, function (err, data) {
-              if (err) {
-                  throw err;
-              }
-
-              var md = marked(data.toString());
-
-              var route = fullName.replace('.md', '');
-              var route = route.replace('routes', '');
-
-              app.get(route, function(request, response) {
-                var locals = { appName : "Decoded Brand Guidelines", md: md};
-                response.render('pages/md', locals);
-              });
-          });
-
-
+        // if the file is markdown, generate a MD route
+          markdownRoute(fullName);
         }
     });
 }
-recursiveRoutes('routes'); // Initialize it
+
+// read the md file, parse it and render the file in MD template
+function markdownRoute(routePath){
+  fs.readFile(routePath, function (err, data) {
+      if (err) {
+          throw err;
+      }
+      var md = marked(data.toString());
+      // formate route string
+      var route = routePath.replace('.md', '');
+      var route = route.replace('routes', '');
+      // init route
+      app.get(route, function(request, response) {
+        var locals = { appName : "Decoded Brand Guidelines", md: md};
+        response.render('pages/md', locals);
+      });
+  });
+}
+recursiveRoutes('pages'); // Initialize it
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ src/pages/typography.md
@@ -0,0 +1,7 @@
+# Typography
+
+## Font
+The Source Pro Family of fonts give Decoded communications and products a consistent appearance and transport our values with ease.
+It is used across all print, digital and all other media. The Source Pro Family is a high-quality open source font system  that can unite the brand appearance of Decoded across all channels and is available on all platforms Decoded is using to deliver our products.
+
+Decoded’s primary typeface is  Source Sans Pro.
